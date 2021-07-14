b5f86f5014d94b292d9c3e2b9cc2556606491b95
Moira Leon <86265787+moiraleon@users.noreply.github.com>
2021-07-14 09:59:01 -0600

    Add files via upload

---
+++ Cats.sql
@@ -0,0 +1,19 @@
+Cats 
+
+-- create table cats(
+--   cats_id serial primary key,
+--   name VARCHAR(20),
+--   color VARCHAR(20),
+--   favorite_toy VARCHAR(20),
+--   age Integer)
+  
+--   insert into cats
+--   values (1,'Tom', 'orange', 'floppy-fish',4)
+--   insert into cats
+--   values (2,'Travis Scott', 'striped', 'microphone', 3);
+--   insert into cats
+--   values (3,'Kyliecat', 'black', 'chicken', 2);
+--   insert into cats
+--   values (4,'KimmyCat', 'white', 'lazer', 5);
+--   insert into cats
+--   values (5,'Kanyecat', 'purple', 'ball', 2)
